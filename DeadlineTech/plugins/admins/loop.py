53c05f0a2a8db3d0d89790119ef265729ff0091f
Team DeadlineTech <tosu80018@gmail.com>
2025-05-23 01:48:56 +0530

    Update loop.py

--- DeadlineTech/plugins/admins/loop.py
+++ DeadlineTech/plugins/admins/loop.py
@@ -3,10 +3,10 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_loop, set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from DeadlineTech import app
+from DeadlineTech.utils.database import get_loop, set_loop
+from DeadlineTech.utils.decorators import AdminRightsCheck
+from DeadlineTech.utils.inline import close_markup
 from config import BANNED_USERS
 
 
