b7da8ab383236f5b758eb00c792c18c39f247d8c
Team DeadlineTech <tosu80018@gmail.com>
2025-05-23 01:49:47 +0530

    Update resume.py

--- DeadlineTech/plugins/admins/resume.py
+++ DeadlineTech/plugins/admins/resume.py
@@ -3,11 +3,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_on
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from DeadlineTech import app
+from DeadlineTech.core.call import Anony
+from DeadlineTech.utils.database import is_music_playing, music_on
+from DeadlineTech.utils.decorators import AdminRightsCheck
+from DeadlineTech.utils.inline import close_markup
 from config import BANNED_USERS
 
 
