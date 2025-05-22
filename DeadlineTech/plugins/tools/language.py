e9254b9899fc6cbdd54cfa47031f61c4ca6c46cf
Team DeadlineTech <tosu80018@gmail.com>
2025-05-23 02:18:57 +0530

    Update language.py

--- DeadlineTech/plugins/tools/language.py
+++ DeadlineTech/plugins/tools/language.py
@@ -2,9 +2,9 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_lang, set_lang
-from AnonXMusic.utils.decorators import ActualAdminCB, language, languageCB
+from DeadlineTech import app
+from DeadlineTech.utils.database import get_lang, set_lang
+from DeadlineTech.utils.decorators import ActualAdminCB, language, languageCB
 from config import BANNED_USERS
 from strings import get_string, languages_present
 
