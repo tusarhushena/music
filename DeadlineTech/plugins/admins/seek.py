e89c7604927ec380ae0d66d0ccf01b8557acc7ad
Team DeadlineTech <tosu80018@gmail.com>
2025-05-23 01:50:10 +0530

    Update seek.py

--- DeadlineTech/plugins/admins/seek.py
+++ DeadlineTech/plugins/admins/seek.py
@@ -3,11 +3,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AdminRightsCheck, seconds_to_min
-from AnonXMusic.utils.inline import close_markup
+from DeadlineTech import YouTube, app
+from DeadlineTech.core.call import Anony
+from DeadlineTech.misc import db
+from DeadlineTech.utils import AdminRightsCheck, seconds_to_min
+from DeadlineTech.utils.inline import close_markup
 from config import BANNED_USERS
 
 
