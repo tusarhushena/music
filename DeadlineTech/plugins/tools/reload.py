024be57895f3e66b8875fc649c5646fef357eacf
Team DeadlineTech <tosu80018@gmail.com>
2025-05-23 02:20:24 +0530

    Update reload.py

--- DeadlineTech/plugins/tools/reload.py
+++ DeadlineTech/plugins/tools/reload.py
@@ -5,12 +5,12 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
-from AnonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
-from AnonXMusic.utils.formatters import alpha_to_int, get_readable_time
+from DeadlineTech import app
+from DeadlineTech.core.call import Anony
+from DeadlineTech.misc import db
+from DeadlineTech.utils.database import get_assistant, get_authuser_names, get_cmode
+from DeadlineTech.utils.decorators import ActualAdminCB, AdminActual, language
+from DeadlineTech.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 
 rel = {}
