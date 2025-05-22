f4e5853d8ca26955744b445a940cdd8b217343ed
Team DeadlineTech <tosu80018@gmail.com>
2025-05-23 01:57:04 +0530

    Update channel.py

--- DeadlineTech/plugins/play/channel.py
+++ DeadlineTech/plugins/play/channel.py
@@ -4,9 +4,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils.database import set_cmode
-from AnonXMusic.utils.decorators.admins import AdminActual
+from DeadlineTech import app
+from DeadlineTech.utils.database import set_cmode
+from DeadlineTech.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
